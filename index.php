374f77e582e81d9defbf6387085cf013d65ea4f7
Mussavir Shahid <shahid.muhammad@student.greenriver.com>
2023-04-24 01:57:05 -0400

    few style changes

--- index.php
+++ index.php
@@ -15,17 +15,14 @@ require_once ('vendor/autoload.php');
 // Create an F3 (Fat Free Framework) object
 $f3 = Base::instance();
 
-/*
 // Define a default route
-$F3->route('GET /', function (){
-   echo '<h1>Hello, World!</h1>';
-*/
+$f3->route('GET /', function() {
+    echo 'Hello, World!';
+});
+
+
 $view = new Template();
 echo $view->render('views/home.html');
 
-/*$f3->route('GET /', function (){
-    echo "Hello, World";
-});
-*/
 // Run Fat Free Framework
 $f3->run();
