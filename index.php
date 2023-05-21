b22c0371cac51ffcd1e6af438dfd8a265ff2b2c4
Mussavir Shahid <shahid.muhammad@student.greenriver.com>
2023-05-21 00:57:23 -0400

    adjusted summary.html file to get data from input

--- index.php
+++ index.php
@@ -24,6 +24,8 @@ $f3->route('GET /', function() {
 
 });
 
+session_start();
+
 //Define a a personal info route
 
 $f3->route('GET /personalinfo', function() {
@@ -46,20 +48,20 @@ $f3->route('GET|POST /personalinfo', function($f3) {
        //array(5) { ["first_name"]=> string(8) "Muhammad" ["last_name"]=> string(6) "Shahid" ["email"]=> string(24)
         // "mussavirshahid@gmail.com" ["state"]=> string(10) "WASHINGTON" ["phone"]=> string(10) "4242099636" }
 
-        /*$firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
+        $firstname = $_POST['first_name'];
+        $lastname = $_POST['last_name'];
         $email = $_POST['email'];
         $state = $_POST['state'];
-        $phone = $_POST['phone'];*/
+        $phone = $_POST['phone'];
 
         // Validate the data
 
         // Store the data in the session array
-        /*$f3->set('SESSION.firstname', $firstname);
+        $f3->set('SESSION.firstname', $firstname);
         $f3->set('SESSION.lastname', $lastname);
         $f3->set('SESSION.email', $email);
         $f3->set('SESSION.state', $state);
-        $f3->set('SESSION.phone', $phone);*/
+        $f3->set('SESSION.phone', $phone);
 
         // Redirect to experience route
         $f3->reroute('experience');
@@ -81,21 +83,21 @@ $f3->route('GET|POST /experience', function($f3) {
         echo var_dump();*/
        /* array(5) { ["bio"]=> string(2) "df" ["github"]=> string(2) "om" ["years"]=> string(1) "2" ["relocate"]=>
          string(3) "yes" ["submit"]=> string(0) "" }*/
-        /*$bio = $_POST["bio"];
+        $bio = $_POST["bio"];
         $github = $_POST['github'];
         $years = $_POST['years'];
         $relocate = $_POST['relocate'];
-*/
+
 
         //Validate the data
 
         //Store the data in the session array
-        /*
+
         $f3->set('SESSION.bio', $bio);
         $f3->set('SESSION.github', $github);
         $f3->set('SESSION.years', $years);
         $f3->set('SESSION.relocate', $relocate);
-        */
+
         //Redirect to the summary route
         $f3->reroute('mailing-list');
     }
@@ -118,16 +120,16 @@ $f3->route('GET|POST /mailing-list', function($f3) {
         /*array(3) { ["software_jobs"]=> array(3) { [0]=> string(10) "JavaScript" [1]=> string(3) "PHP" [2]=>
         string(4) "Java" } ["industry_verticals"]=> array(3) { [0]=> string(4) "Saas" [1]=> string(7) "Ag tech" [2]=>
         string(13) "Cybersecurity" } ["submit"]=> string(0) "" }*/
-        /*$software_jobs = implode(", ", $_POST['software_jobs']);
-        $industry_verticals = implode(", ", $_POST['industry_verticals']);*/
+        $software_jobs = implode(", ", $_POST['software_jobs']);
+        $industry_verticals = implode(", ", $_POST['industry_verticals']);
 
 
         //Validate the data
 
         //Store the data in the session array
 
-       /* $f3->set('SESSION.software_jobs', $software_jobs);
-        $f3->set('SESSION.industry_verticals', $industry_verticals);*/
+        $f3->set('SESSION.software_jobs', $software_jobs);
+        $f3->set('SESSION.industry_verticals', $industry_verticals);
 
         //Redirect to the summary route
         $f3->reroute('summary');
@@ -146,7 +148,7 @@ $f3->route('GET /summary', function() {
     $view = new Template();
     echo $view->render('views/summary.html');
 
-    /*session_destroy();*/
+    session_destroy();
 });
 
 // Run Fat Free Framework
