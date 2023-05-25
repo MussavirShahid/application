3a98d6ea11a59e4b36fceb667a0071324fe2210a
Mussavir Shahid <shahid.muhammad@student.greenriver.com>
2023-05-25 01:33:32 -0400

    added data layer file

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,1 +1,22 @@
 <?php
+/*  328/application/model/data-layer.php
+    Returns data for the application
+    This is part of the MODEL
+    Eventually, this will read/write the DB
+*/
+
+class DataLayer
+{
+
+
+    static function job()
+    {
+        $job = array("JavaScript", "PHP", "Python", "HTML", "CSS", "ReactJS", "NodeJS");
+        return $job;
+    }
+    static function vertical()
+    {
+        $vertical = array("Saas", "Health tech", "Ag tech", "HR tech", "Industrial tech", "Cybersecurity");
+        return $vertical;
+    }
+}
